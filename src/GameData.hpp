98bd8d1b46fee210a47dfb0591c7b2b78320c49a
krzyurb <krzyurb@gmail.com>
2016-03-13 22:08:29 +0100

    GameData - json parser class

---
+++ src/GameData.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+class GameData {
+public:
+    template<typename T>
+    static T read(std::string fileName, std::string key){
+        T value;
+        try
+        {
+            boost::property_tree::ptree pt;
+            read_json("data/"+fileName+".mtd", pt);
+            value = pt.get<T>(key);
+        }
+        catch (std::exception const&e)
+        {
+            std::cerr << "GameData error: " << e.what() << std::endl;
+        }
+        return value;
+    }
+};
