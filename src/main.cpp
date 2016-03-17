d46dbc264e50457898e5645a836633a012078015
davpal <paluchdaw@gmail.com>
2016-03-17 20:39:18 +0100

    Add SFML to cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Modus tollens");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        // window.draw(...);
+        sf::CircleShape circle(100, 100);
+        window.draw(circle);
+
+        // end the current frame
+        window.display();
+    }
+
     return 0;
 }
