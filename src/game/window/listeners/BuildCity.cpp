6b2461e00319a8cc54ae46906d9d5152b01bb62e
krzyurb <krzyurb@gmail.com>
2016-05-10 14:20:28 +0200

    Add city field resources

--- src/game/window/listeners/BuildCity.cpp
+++ src/game/window/listeners/BuildCity.cpp
@@ -5,10 +5,9 @@
 
 
 void BuildCity::perform() {
-    std::cout << "Builded City!" << std::endl;
     Field * field = getFieldSelector()->getSelectedField();
-    Field * city = new City(field->getX(), field->getY(), field->getWorld());
-    getFieldSelector()->getSelectedField()->getWorld()->setField(getFieldSelector()->getSelectedField()->getId(), city);
+    Field * city  = new City(field->getX(), field->getY(), field->getWorld());
+    field->getWorld()->setField(field->getId(), city);
 }
 
 BuildCity::BuildCity(FieldSelector *fieldSelector) : fieldSelector(fieldSelector) {}
