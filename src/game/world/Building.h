1d7df25493324ff19c4b9d81fe56b9d205b5af81
krzyurb <krzyurb@gmail.com>
2016-05-15 22:03:03 +0200

    Create Building class

---
+++ src/game/world/Building.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Field.h"
+
+class Building {
+public:
+
+    Building(Field *field) : field(field) {}
+
+    Field *getField() const {
+        return field;
+    }
+
+    void setField(Field *field) {
+        Building::field = field;
+    }
+
+    const std::string &getKind() const {
+        return kind;
+    }
+
+    void produce() {}
+
+private:
+    Field * field;
+    std::string kind;
+};
