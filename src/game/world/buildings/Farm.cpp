6cc0cc3e86cd7fdb43f2aa60a0f650a30b06b11d
krzyurb <krzyurb@gmail.com>
2016-05-28 13:16:46 +0200

    Refactor Farm.cpp

--- src/game/world/buildings/Farm.cpp
+++ src/game/world/buildings/Farm.cpp
@@ -2,15 +2,16 @@
 #include "Farm.h"
 
 void Farm::produce(Player * player) {
-    std::string fieldKind = getField()->getKind();
+    if(getField()->getKind() == "meadow"){
+        Meadow * field = dynamic_cast<Meadow*>(getField());
+        Soil   * soil  = dynamic_cast<Soil*>(&field->getSoil());
+        if(field == nullptr || soil == nullptr) return;
 
-    if(fieldKind == "meadow"){
-        Meadow * field = (Meadow*) getField();
-        Soil soil = field->getSoil();
-        int fertility = soil.getFertility();
+        int fertility = soil->getFertility();
         int food = (fertility / 2) + (std::rand() % ( fertility - (fertility / 2) + 1 ));
+
         if(player->getStock().tools >= (fertility /10)) {
-            player->getStock().food += food / 10;
+            player->getStock().food  += food / 10;
             player->getStock().tools -= fertility / 10;
         }
     }
