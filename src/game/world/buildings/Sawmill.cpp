643afed37d0bb9b8ca0914d9dbc8a8338ed04ced
krzyurb <krzyurb@gmail.com>
2016-05-28 13:34:39 +0200

    Refactor Sawmill.cpp

--- src/game/world/buildings/Sawmill.cpp
+++ src/game/world/buildings/Sawmill.cpp
@@ -1,31 +1,26 @@
 #include "Forest.h"
 #include "Sawmill.h"
-#include <iostream>
 
 void Sawmill::produce(Player * player) {
-    std::string fieldKind = getField()->getKind();
-    if(fieldKind == "forest"){
-        Forest * field = (Forest *) getField();
+    if(getField()->getKind() == "forest"){
+        Forest * field = dynamic_cast<Forest *>(getField());
+        if(field == nullptr || field->getTreesCount() <= 0) return;
+        if(field->getTreesCount() < 0) field->setTreesCount(0);
 
-        if(field->getTreesCount() > 0){
-            int endurance = field->getTree().getEndurance() / 10;
-            int wood = (endurance / 2) + (std::rand() % ( endurance - (endurance / 2) + 1 ));
+        int endurance = field->getTree().getEndurance() / 10;
+        int wood = (endurance / 2) + (std::rand() % ( endurance - (endurance / 2) + 1 ));
+        int extraWood = 0;
+        bool deepForest = true;
 
-            bool deepForest = true;
-            for(auto & neighbor: getField()->getNeighbors()) {
-                if(neighbor->getKind() != "forest")
-                    deepForest = false;
-            }
-            if(deepForest) { wood += 4; }
+        for(auto & neighbor: getField()->getNeighbors()) {
+            if (neighbor->getKind() != "forest") deepForest = false;
+        }
+        if(deepForest) extraWood+= 4;
 
-            if(player->getStock().tools >= endurance){
-                field->cutTrees(wood);
-                player->getStock().wood  += wood;
-                player->getStock().tools -= endurance;
-            } else {
-            }
-        } else {
-            field->setTreesCount(0);
+        if(player->getStock().tools >= endurance){
+            field->cutTrees(wood);
+            player->getStock().wood  += (wood + extraWood);
+            player->getStock().tools -= endurance;
         }
     }
 }
